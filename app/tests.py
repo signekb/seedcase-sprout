c3db1f143c3d2a0a8356c535284a3fdbd1539f1d
Philip Hjorth Christiansen <pch@mjolner.dk>
2024-01-12 11:42:50 +0100

    Github action running tests

--- app/tests.py
+++ app/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
 
-# Create your tests here.
+# Note: Two types of TestCase: django.test or unittest
+# https://docs.djangoproject.com/en/5.0/topics/testing/overview/
+class SmokeTest(TestCase):
+    def test_smoke(self):
+        self.assertTrue(True)
+
