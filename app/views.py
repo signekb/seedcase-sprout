594e78e897ad75f5d569f9f9dbd89aff89d4494d
Luke W. Johnston <lwjohnst@gmail.com>
2024-01-15 17:14:19 +0100

    fix: :coffin: Seems like this code wasn't used, so removed it

--- app/views.py
+++ app/views.py
@@ -1,10 +1,7 @@
 from django.shortcuts import render
-from datetime import datetime
 
 def home(request):
     return render(request, "home.html")
 
-
 def data_import(request):
-    now = datetime.now()
     return render(request, "data-import.html")
