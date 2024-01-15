5c7503e44824e52b342790d4fa9fc33eb66641bc
Luke W. Johnston <lwjohnst@gmail.com>
2024-01-15 17:15:18 +0100

    style: :art: Ran Ruff styling

--- app/urls.py
+++ app/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('data-import', views.data_import, name='data_import')
+    path("", views.home, name="home"),
+    path("data-import", views.data_import, name="data_import"),
 ]
