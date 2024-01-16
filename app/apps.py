610ae7388ea3ef1cb4e87f1e124180cc619f4075
Philip Hjorth Christiansen <pch@mjolner.dk>
2024-01-16 14:27:33 +0100

    fix: Reverting changes related to enforcing foreign key constraint in SQLite. This seems to be handled by Django

--- app/apps.py
+++ app/apps.py
@@ -1,17 +1,6 @@
 from django.apps import AppConfig
-from django.db.backends.signals import connection_created
-
-
-def activate_foreign_keys(sender, connection, **kwargs):
-    """Enable integrity constraint with sqlite."""
-    if connection.vendor == 'sqlite':
-        cursor = connection.cursor()
-        cursor.execute('PRAGMA foreign_keys = ON;')
 
 
 class AppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'app'
-
-    def ready(self):
-        connection_created.connect(activate_foreign_keys)
