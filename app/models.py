591221faf174fd1cb731c3d0c028d38bd9bd89b9
Philip Hjorth Christiansen <pch@mjolner.dk>
2024-01-16 11:54:51 +0100

    feat: Models added for table and column meta data

--- app/models.py
+++ app/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+
+class TableMetadata(models.Model):
+    name = models.CharField(max_length=128)
+    original_file_name = models.CharField(max_length=200)
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    created_by = models.IntegerField()
+
+
+class ColumnDataType(models.Model):
+    display_name = models.TextField()
+
+
+class ColumnMetadata(models.Model):
+    table_metadata = models.ForeignKey(TableMetadata, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    data_type = models.ForeignKey(ColumnDataType, on_delete=models.PROTECT)
+    allow_missing_value = models.BooleanField()
+    allow_duplicate_value = models.BooleanField()
--- app/views.py
+++ app/views.py
@@ -2,8 +2,23 @@ from django.shortcuts import render
 from datetime import datetime
 from django.http import HttpResponse
 
+from app.models import TableMetadata, ColumnMetadata, ColumnDataType
+
 
 def index(request):
+    tables = TableMetadata.objects.all()
+    print('Table row count before', tables.count())
+
+    t = TableMetadata(name="Tabel1", original_file_name="my.csv", description="First table", created_by=1)
+    t.save()
+
+    dt = ColumnDataType(display_name="Text")
+    dt.save()
+
+    c = ColumnMetadata(table_metadata=t, name="CPR", title="CPR", description="id", data_type=dt, allow_missing_value=True, allow_duplicate_value=True)
+    c.save()
+
+    print('Table row count after', tables.count())
     return HttpResponse("Hello go to /hello to render a template")
 
 
