1889919199e4f0b42896246627fac0ff5d50a79c
Philip Hjorth Christiansen <pch@mjolner.dk>
2024-01-18 10:00:24 +0100

    feat: draft

--- app/urls.py
+++ app/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path("", views.home, name="home"),
     path("data-import", views.data_import, name="data_import"),
+    path('create-new-table', views.create_new_table, name='create_new_table')
 ]
--- app/views.py
+++ app/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
 
+
 def home(request):
     return render(request, "home.html")
 
+
 def data_import(request):
     return render(request, "data-import.html")
+
+
+def create_new_table(request):
+
+    return render(request, "create-new-table.html", {'table_name': 'Philip'})
