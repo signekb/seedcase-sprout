19c0c3851ba615ccd4f1115a45a4aa979797e6d1
Philip Hjorth Christiansen <pch@mjolner.dk>
2024-01-23 16:00:19 +0100

    feat: added dropzone and file information box

--- app/tests/tests_views.py
+++ app/tests/tests_views.py
@@ -33,3 +33,26 @@ class FileUploadTests(TestCase):
         self.assertEqual("/edit-table-columns/1", response.url)
         columns = ColumnMetadata.objects.all()
         self.assertEqual(3, columns.count(), "Three columns are created")
+
+    def test_upload_failed_with_wrong_file_extension(self):
+        create_table("Table Name").save()
+        file = SimpleUploadedFile("file-with-wrong-ext.svg", b"file content")
+
+        response = Client().post("/file-upload/1", {"uploaded_file": file})
+
+        self.assertContains(response, "Unsupported file format: .svg")
+
+    def test_upload_failed_with_missing_file_error(self):
+        create_table("Table Name").save()
+
+        response = Client().post("/file-upload/1")
+
+        self.assertContains(response, "The file is missing!")
+
+    def test_upload_failed_with_invalid_csv_header(self):
+        create_table("Table Name").save()
+        file = SimpleUploadedFile("file-with-bad-headers.csv", b"no valid headers")
+
+        response = Client().post("/file-upload/1", {"uploaded_file": file})
+
+        self.assertContains(response, "Unable to extract column headers")
--- app/views/file_upload.py
+++ app/views/file_upload.py
@@ -3,9 +3,42 @@ from django.shortcuts import render, redirect
 from app.models import ColumnMetadata, TableMetadata
 
 
+def file_upload(request, table_id):
+    if request.method == "GET":
+        return render_file_upload_page(request, table_id, "")
+
+    upload_error = extract_columns_and_get_error(table_id, request.FILES)
+    if upload_error:
+        return render_file_upload_page(request, table_id, upload_error)
+
+    return redirect("/edit-table-columns/" + str(table_id))
+
+
+def render_file_upload_page(request, table_id, upload_error):
+    table_metadata = TableMetadata.objects.get(pk=table_id)
+    file_upload_data = {"table_name": table_metadata.name, "upload_error": upload_error}
+    return render(request, "file-upload.html", file_upload_data)
+
+
+def extract_columns_and_get_error(table_id, files):
+    # File is missing
+    uploaded_file = files.get("uploaded_file", None)
+    if not uploaded_file:
+        return "The file is missing!"
+
+    if not uploaded_file.name.endswith(".csv"):
+        return "Unsupported file format: ." + uploaded_file.name.split(".")[-1]
+
+    return extract_and_persist_column_metadata(table_id, uploaded_file)
+
+
 def extract_and_persist_column_metadata(table_id, uploaded_file):
     # A more complicated function needs to be added
     column_names = uploaded_file.readline().decode("utf-8").split(",")
+
+    if len(column_names) < 2:
+        return "Unable to extract column headers"
+
     for name in column_names:
         ColumnMetadata(
             table_metadata_id=table_id,
@@ -16,19 +49,4 @@ def extract_and_persist_column_metadata(table_id, uploaded_file):
             allow_missing_value=True,
             allow_duplicate_value=True,
         ).save()
-
-
-def file_upload(request, table_id):
-    table_metadata = TableMetadata.objects.get(pk=table_id)
-    file_upload_data = {"table_name": table_metadata.name, "upload_error": ""}
-    if request.method == "GET":
-        return render(request, "file-upload.html", file_upload_data)
-
-    # File is missing
-    if "uploaded_file" not in request.FILES:
-        file_upload_data["upload_error"] = "MissingFile"
-        return render(request, "file-upload.html", file_upload_data)
-
-    uploaded_file = request.FILES["uploaded_file"]
-    extract_and_persist_column_metadata(table_id, uploaded_file)
-    return redirect("/edit-table-columns/" + str(table_id))
+    return ""
