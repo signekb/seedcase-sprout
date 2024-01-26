51d8bbdceb3ec73b8d0a63a06e4b41eccfed70d8
Signe Kirk Brødbæk <signekb@clin.au.dk>
2024-01-26 13:34:26 +0100

    style: add and remove blank lines

--- app/views.py
+++ app/views.py
@@ -4,15 +4,17 @@ from django.shortcuts import render
 from app.forms import TableMetadataForm
 from app.validators.model_dependent_validators import does_table_name_exist_in_db
 
+
 def home(request):
     return render(request, "home.html")
+
 
 def data_import(request):
     # if this is a POST request, we need to process the form data
     if request.method == "POST":
-
         # create a form instance and populate it with data from the request
         form = TableMetadataForm(request.POST)
+
         # if user input passes validation
         if form.is_valid():
             # extra val: does table name exist in db
@@ -31,8 +33,9 @@ def data_import(request):
             table = form.save()
 
             return HttpResponseRedirect(f"/file-upload/{table.id}")
+
     # if GET (or any other method) we'll create a blank form
     else:
         form = TableMetadataForm()
-    return render(request, "data-import.html", {"form": form})
 
+    return render(request, "data-import.html", {"form": form})
