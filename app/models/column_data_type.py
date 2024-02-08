0ecfb3659193503da41ffac442747670bdc60b07
Philip Hjorth Christiansen <pch@mjolner.dk>
2024-02-08 09:51:05 +0100

    fix: The columns are updated after a migration

--- app/models/column_data_type.py
+++ app/models/column_data_type.py
@@ -1,5 +1,3 @@
-from typing import Any
-
 from django.db import models
 
 
@@ -10,69 +8,52 @@ class ColumnDataType(models.Model):
     pandas_type = models.TextField()
 
 
-def update_column_data_types(*_: Any) -> None:
-    """
-    Method to be used in migrations when the content of COLUMN_DATA_TYPES
-    is changed in any way.
-
-    See COLUMN_DATA_TYPES docstring for more info
-
-    Args:
-        *_: An argument not used, but required by the Django Framework
-    """
-    ColumnDataType.objects.bulk_create(COLUMN_DATA_TYPES, ignore_conflicts=True)
-    fields = ['display_name', 'description', 'python_type', 'pandas_type']
-    ColumnDataType.objects.bulk_update(COLUMN_DATA_TYPES, fields=fields)
-
-
-def fake_reverting_method(*_: Any):
-    """
-    This method is used in migrations when COLUMN_DATA_TYPES is changed.
-    Django expects a method for reverting a migration, so that is why we have created
-    this "empty" method.
-
-    Since this method is empty, the reverting of a migration will not have an effect on
-    the table app_columndatatype
-
-    Args:
-        *_: An argument not used, but required by the Django Framework
-    """
-    print("Reverting ")
-
-
 """
-When the contents of COLUMN_DATA_TYPES is changed, you need to add a new migration.
-You create a migration with:
+COLUMN_DATA_TYPES contains all the "sprout" types.
 
-python manage.py makemigrations
+Changing the properties of the content can be risky. See the list below
 
-A migration file is added in the migrations folder. Add the following
-line at the end of the file:
-
-migrations.RunPython(update_column_data_types, fake_reverting_method)
-
-This will update the list of ColumnDataType defined in COLUMN_DATA_TYPES.
-(See 00001_initial.py for inspiration)
+- NO RISK: The `description` is safe to change
+- LITTLE RISK: The `display_name` should rarely change. Changes will confuse the user and
+  should never change to a different type (Text --> Decimal)
+- HIGH RISK: The properties `pandas_type` and `python_type` should rarely change. It
+  affects how we parse CSV files
+- FORBIDDEN: id. You should never change the id
 """
 COLUMN_DATA_TYPES = [
     ColumnDataType(id=0, display_name="Decimal",
                    description="Also known as a Float or Double Precision. This field stores decimal numbers. Use this for items like height, blood glucose, or other measurements with high degrees of precision",
                    pandas_type="float64", python_type="float"),
-    ColumnDataType(id=1, display_name="Whole Number", pandas_type="int64",python_type="int",
-                   description="Also known as an `integer`. This field only allows whole numbers to be stored. Use this when you want to store data that doesn't need a decimal (e.g. number of people in a household)"),
+    ColumnDataType(id=1, display_name="Whole Number",
+                   description="Also known as an `integer`. This field only allows whole numbers to be stored. Use this when you want to store data that doesn't need a decimal (e.g. number of people in a household)",
+                   pandas_type="int64", python_type="int"),
     ColumnDataType(id=2, display_name="Text",
                    description="A text field without a limit to how many characters can be stored",
                    pandas_type="object", python_type="str"),
     ColumnDataType(id=3, display_name="Yes/No",
                    description="Also known as Yes/No, Boolean, or Logical. This stores data as either 0/1 or True/False. If you need/want to store actual words, use the Text field",
-                   pandas_type="bool",
-                   python_type="bool"),
+                   pandas_type="bool", python_type="bool"),
     ColumnDataType(id=4, display_name="Time",
                    description="Stores time following the [ISO 8601](https://www.iso.org/iso-8601-date-and-time-format.html) format of `hh:mm:ss.ms` (hours, minutes, seconds, milliseconds)",
-                   pandas_type="timedelta64[ns]",
-                   python_type="timedelta"),
+                   pandas_type="timedelta64[ns]", python_type="timedelta"),
     ColumnDataType(id=5, display_name="Date+Time",
                    description="Also known as DateTime or Timestamp. The field stores the date time following the [ISO 8601](https://www.iso.org/iso-8601-date-and-time-format.html) format as `yyyy-mm-dd hh:mm:ss.ms` (year, month, day, hour, minutes, seconds, milliseconds)",
-                   pandas_type="datetime64[ns]",
-                   python_type="datetime")
+                   pandas_type="datetime64[ns]", python_type="datetime")
 ]
+
+
+def update_column_data_types(**kwargs) -> None:
+    """
+    Creates or updates all ColumnDataType in the database based on the content of
+    COLUMN_DATA_TYPES.
+
+    This is executed on app startup just after the migrations are completed. This method
+    is used in AppConfig.ready()
+
+    Args:
+        **kwargs: A required argument by the Django framework, but it is not used
+    """
+    print("ColumnDataType updated")
+    ColumnDataType.objects.bulk_create(COLUMN_DATA_TYPES, ignore_conflicts=True)
+    fields = ['display_name', 'description', 'python_type', 'pandas_type']
+    ColumnDataType.objects.bulk_update(COLUMN_DATA_TYPES, fields=fields)
