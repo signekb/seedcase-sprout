6ecf6584991e6f549f49af735285e62e3c0cdac9
Kris Beicher <112945740+K-Beicher@users.noreply.github.com>
2024-02-09 11:27:52 +0100

    add table name and name from data type to view

--- app/views/column_review.py
+++ app/views/column_review.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render #, redirect
 
-from app.models import ColumnMetadata, ColumnDataType
+from app.models import ColumnMetadata, ColumnDataType, TableMetadata
 
 def column_review(request):
-  cols = ColumnMetadata.objects
-  return render(request, 'column-review.html', {'cols':cols})
+  # Retrieve data with join using select_related
+  columns_data = ColumnMetadata.objects.select_related('data_type', 'table_metadata').all()
+  return render(request, 'column-review.html', {'cols':columns_data})
 
 def columndata_review(request):
-  colsd = ColumnDataType.objects
-  return render(request, 'columndata-review.html', {'colsdata':colsd})
+  colsdata = ColumnDataType.objects
+  return render(request, 'columndata-review.html', {'colsdata':colsdata})
