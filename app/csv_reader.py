2fe0d4fdb6edd94082aed458fde99a6a1e162b72
Philip Hjorth Christiansen <pch@mjolner.dk>
2024-02-12 10:26:41 +0100

    feat: read csv file with polars

---
+++ app/csv_reader.py
@@ -0,0 +1,105 @@
+import csv
+import io
+from typing import TextIO, Any
+
+from polars import read_csv, Boolean, Series, DataFrame
+
+
+def read_csv_file(csv_file: TextIO, row_number: int = 1000):
+    """
+    Reads a CSV file and returns a polars.DataFrame with column/series types in dtypes.
+
+    It uses `polars.csv_read()`, but adds additional functionality:
+    - Converts boolean-ish values (Yes, y, 1) to booleans
+    - Finds CSV dialect of file and converts to a csv format suitable for
+      polars.csv_read()
+
+    Args:
+        csv_file: The CSV file to read
+        row_number: The number of rows to scan from the file
+
+    Returns:
+        DataFrame: A polars.DataFrame with column types in `dtypes`.
+    """
+    csv_file = _transform_to_suitable_csv_format(csv_file, row_number)
+    df = read_csv(csv_file, n_rows=row_number, try_parse_dates=True, separator=';')
+
+    return _convert_to_boolean_series_if_possible(df)
+
+
+def _transform_to_suitable_csv_format(csv_file: TextIO, row_number: int) -> TextIO:
+    """
+    Preparing the CSV content for polar.read_csv method. The method `polar.read_csv`
+    is not able to parse some CSV dialects.
+
+    This method converts from any CSV dialect/format into a CSV format suitable for
+    polar.read_csv
+
+    Args:
+        csv_file: A CSV file with a dialect that is potentially not suitable for
+                  polars.read_csv
+        row_number: The number of rows to transform
+
+    Returns:
+        TextIO: An in-memory CSV file which is suitable for polars.read_csv()
+    """
+
+    # Read part of csv_file to detect dialect and return to beginning of file
+    dialect = csv.Sniffer().sniff(csv_file.read(row_number))
+    csv_file.seek(0)
+
+    csv_content = ''
+    for line in csv.reader(csv_file, dialect=dialect):
+        csv_content = csv_content + ';'.join(line) + '\n'
+    return io.StringIO(csv_content)
+
+
+def _convert_to_boolean_series_if_possible(df: DataFrame) -> DataFrame:
+    """
+    Converts DataFrame series of boolean_ish values (0,1,"Yes","No") to
+    booleans (True/False)
+
+    Args:
+        df: Polars Dataframe with series
+
+    Returns:
+        DataFrame: A DataFrame with series converted to
+    """
+    boolean_mapping = {"yes": True, "Yes": True, "y": True, "Y": True,
+                       "no": False, "No": False, "n": False, "N": False}
+
+    for column in df.columns:
+        series = df[column]
+        if _check_series_values(series, [0, 1]):
+            df = df.with_columns(series.cast(Boolean))
+        elif _check_series_values(series, list(boolean_mapping.keys())):
+            bool_series = series.map_dict(boolean_mapping).cast(Boolean)
+            df = df.with_columns(bool_series)
+
+    return df
+
+
+def _check_series_values(series: Series, allowed_values: list[Any]) -> bool:
+    """
+    Checks if a series only contains the allowed values and null values. This is
+    relevant check before converting to a certain type.
+
+    E.g. if is all values are either 0 or 1, then we can convert to booleans
+
+    Args:
+        series: A series of values, which should be checked
+        allowed_values: The allowed values.
+
+    Returns:
+        true: if series only contains values in allowed_values or null
+        false: if series contains one or more values not in allowed_values
+
+    """
+    allowed_values_series = Series(allowed_values)
+    if series.dtype != allowed_values_series.dtype:
+        return False
+
+    # Count allowed and null values
+    value_match_count = series.is_in(allowed_values_series).sum()
+    null_count = series.is_null().sum()
+    return null_count + value_match_count == len(series)
---
+++ app/tests/tests_csv_reader.py
@@ -0,0 +1,189 @@
+import csv
+import datetime
+import io
+from typing import Any
+from unittest import TestCase
+
+from polars import DataFrame, Series
+
+from app.csv_reader import read_csv_file
+
+
+class CsvTests(TestCase):
+    def test_csv_with_simple_types(self):
+        """
+        Testing that `read_csv_file()` should derive column "i1" as integer, "f1" as float
+        and "b1" as a boolean. The values are also verified.
+        """
+        csv_file = io.StringIO(
+            "i1,f1,b1\n"
+            "1,2.3,true\n"
+            "2,2.4,false\n"
+            "3,2.6,false"
+        )
+
+        df = read_csv_file(csv_file)
+
+        self.assert_types(df, "Int64", "Float64", "Boolean")
+        self.assert_values(df["i1"], 1, 2, 3)
+        self.assert_values(df["f1"], 2.3, 2.4, 2.6)
+        self.assert_values(df["b1"], True, False, False)
+
+    def test_csv_with_semicolon_and_whitespace(self):
+        """
+        Testing a different csv dialect with semicolon and initial whitespace.
+        """
+        csv_file = io.StringIO(
+            "i1;    f1;     b1;     s1\n"
+            "10;    5.3;    True;   Hi\n"
+            "11;    1.0;    False;  Hello\n"
+            "12;    1;      False;  Man"
+        )
+
+        df = read_csv_file(csv_file)
+
+        self.assert_types(df, "Int64", "Float64", "Boolean", "String")
+        self.assert_values(df["i1"], 10, 11, 12)
+        self.assert_values(df["f1"], 5.3, 1, 1)
+        self.assert_values(df["b1"], True, False, False)
+        self.assert_values(df["s1"], "Hi", "Hello", "Man")
+
+    def test_csv_with_semicolon_and_qoutes(self):
+        """
+        Testing a different csv dialect with semicolon and initial whitespace.
+        """
+        csv_file = io.StringIO(
+            '"i1";  "f1";   "b1";       "s1"\n'
+            '"10";  "5.3";  "True";     "Hi, Man"\n'
+            '"11";  "1.0";  "False";    "Hello?"\n'
+            '"12";  "1";    "False";    "What, about"'
+        )
+
+        df = read_csv_file(csv_file)
+
+        self.assert_types(df, "Int64", "Float64", "Boolean", "String")
+        self.assert_values(df["s1"], "Hi, Man", "Hello?", "What, about")
+
+    def test_boolean_ish_values(self):
+        """
+        Testing boolean-ish values are derived as booleans. We also test that empty
+        values in are converted to None
+
+        """
+        csv_file = io.StringIO(
+            "b1,    b2,     b3,     b4\n"
+            "0,     true,   yes,    y\n"
+            "1,     false,  no,     y\n"
+            ",      false,  ,       n"
+        )
+
+        df = read_csv_file(csv_file)
+
+        self.assert_types(df, "Boolean", "Boolean", "Boolean", "Boolean")
+        self.assert_values(df["b1"], False, True, None)
+        self.assert_values(df["b2"], True, False, False)
+        self.assert_values(df["b3"], True, False, None)
+        self.assert_values(df["b4"], True, True, False)
+
+    def test_series_should_remain_if_series_has_some_none_boolean_values(self):
+        """
+        A column should not be converted to booleans if column has values which are not
+        boolean-ish. The columns should remain as Int64 and String columns
+        """
+        csv_file = io.StringIO(
+            "s1,    s2\n"
+            "0,     true\n"
+            "1,     REALLY TRUE!\n"
+            "2,     false"
+        )
+
+        df = read_csv_file(csv_file)
+
+        self.assert_types(df, "Int64", "String")
+        self.assert_values(df["s1"], 0, 1, 2)
+        self.assert_values(df["s2"], "true", "REALLY TRUE!", "false")
+
+    def test_datetime(self):
+        """
+        Testing different date formats (d1, d2, d3) and a single time column (t1).
+
+        NOTICE: d3 (ex: '27. Oct 1987') is not recognized as date
+        """
+        csv_file = io.StringIO(
+            "d1,                    d2,         d3,             t1\n"
+            "1987-10-27 00:00:00,   1987-10-27, 27. Oct 1987,   12:00:00\n"
+            "2000-01-28 12:00:00,   2000-01-28, 28. Jan 2000,   13:00:01\n"
+            "2024-07-01 12:00:01,   2024-07-01, 1. Jul 2024,    00:00:00"
+        )
+
+        df = read_csv_file(csv_file)
+
+        self.assert_types(
+            df,
+            "Datetime(time_unit='us', time_zone=None)",
+            "Date",
+            "String",
+            "Time"
+        )
+        self.assert_values(df["d1"],
+                           datetime.datetime.fromisoformat("1987-10-27"),
+                           datetime.datetime.fromisoformat("2000-01-28T12"),
+                           datetime.datetime.fromisoformat("2024-07-01T12:00:01"))
+        self.assert_values(df["d2"],
+                           datetime.date.fromisoformat("1987-10-27"),
+                           datetime.date.fromisoformat("2000-01-28"),
+                           datetime.date.fromisoformat("2024-07-01"))
+        self.assert_values(df["d3"],
+                           "27. Oct 1987",
+                           "28. Jan 2000",
+                           "1. Jul 2024")
+        self.assert_values(df["t1"],
+                           datetime.time.fromisoformat("12:00:00"),
+                           datetime.time.fromisoformat("13:00:01"),
+                           datetime.time.fromisoformat("00:00:00"))
+
+    def test_wrongly_formatted_csv(self):
+        """
+        Testing a wrongly formatted CSV file with
+        2 columns but three values in a row.
+        A csv.Error is expected
+
+        """
+        csv_file = io.StringIO(
+            "s1,s2\n"
+            "Hello, World, Seedcase"
+        )
+
+        self.assertRaises(csv.Error, read_csv_file, csv_file)
+
+    def assert_types(self, df: DataFrame, *expected_types: str):
+        """
+        Asserts that a DataFrame have the expected_types. This is a shorthand way
+        of testing the types
+
+        Args:
+            df: The DataFrame with data and types
+            *expected_types: a list of types we expect (in order)
+        """
+
+        self.assertEqual(len(df.columns), len(expected_types), "Missing columns!")
+        for column_position in range(0, len(expected_types)):
+            column_name = df.columns[column_position]
+            column_type = str(df.dtypes[column_position])
+            self.assertEqual(
+                expected_types[column_position], column_type, "column:" + column_name
+            )
+
+    def assert_values(self, s: Series, *expected_values: Any):
+        """
+        A shorthand function asserting that a Series contain the `expected_values`.
+
+        Args:
+            s: A Series is a column of data
+            *expected_values: a list of types we expect (in order)
+        """
+        for value_position in range(0, len(s)):
+            value = s[value_position]
+            expected_value = expected_values[value_position]
+            self.assertEqual(value, expected_value,
+                             s.name + ", row:" + str(value_position))
