c90d57d2c9bc23337f4c75b4db7d7ef24762563f
Luke W. Johnston <lwjohnst@gmail.com>
2024-02-18 21:30:55 -0500

    chore: :truck: Renamed folder to the more accurate name 'config'

--- config/urls.py
+++ config/urls.py
@@ -1,4 +1,4 @@
-"""URL configuration for seedcase_sprout project.
+"""URL configuration for Sprout.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
