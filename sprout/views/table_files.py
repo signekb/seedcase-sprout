8f66f93980be635f958545eebd4d64cd07a20c02
Philip Hjorth Christiansen <pch@mjolner.dk>
2024-02-27 15:28:25 +0100

    feat: Files are displayed in a table

--- sprout/views/table_files.py
+++ sprout/views/table_files.py
@@ -7,20 +7,35 @@ from sprout.models.file_metadata import FileMetaData
 
 
 def table_files(request: HttpRequest, table_id: int) -> HttpResponse:
-    """Overview of files for a table."""
+    """Overview of files for a certain table.
+
+    Args:
+        request: The request from the client
+        table_id: The table id
+
+    Returns:
+        HttpResponse: Render of files for a table
+    """
     table_metadata = TableMetadata.objects.get(pk=table_id)
     files = FileMetaData.objects.filter(table_metadata_id=table_id).all()
-    return render(
-        request, "table-files.html", {"files": files, "table": table_metadata}
-    )
+    context = {"files": files, "table": table_metadata}
+    return render(request, "table-files.html", context)
 
 
 def table_file_download(
     request: HttpRequest, table_id: int, file_id: int
 ) -> FileResponse:
-    """Download of file based on a file_id."""
-    # HERE WE CAN CHECK IF THE USER HAVE ACCESS
-    file_metadata = FileMetaData.objects.get(id=file_id)
+    """Download of file based on a file_id.
+
+    Args:
+        request: The request from the client
+        table_id: Table id
+        file_id: The id of the file to download
+
+    Returns:
+        FileResponse: A file download response
+    """
+    file_metadata = FileMetaData.objects.get(id=file_id, table_metadata_id=table_id)
     print(file_metadata.server_file_path)
 
     return FileResponse(open(file_metadata.server_file_path, "rb"), as_attachment=True)
