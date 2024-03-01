5fce586df2207a74d50b09570fe957668d6fd485
Signe Kirk Brødbæk <signekb@clin.au.dk>
2024-03-01 13:51:06 +0100

    test: add tests for _convert_to_snake_case

--- sprout/tests/tests_views.py
+++ sprout/tests/tests_views.py
@@ -5,6 +5,7 @@ from django.urls import reverse
 
 from sprout.models import ColumnMetadata, TableMetadata
 from sprout.tests.db_test_utils import create_table
+from sprout.views.file_upload import _convert_to_snake_case
 
 
 class DataImportTests(TestCase):
@@ -145,6 +146,40 @@ class FileUploadTests(TestCase):
         response = Client().post("/file-upload/1", {"uploaded_file": file})
 
         self.assertContains(response, "Unable to extract column headers")
+
+
+class ConvertToSnakeCase(TestCase):
+    """Tests for the convert_to_snake_case utility function used in file upload"""
+
+    def test_convert_to_snake_case_snake_case(self):
+        string = _convert_to_snake_case("snake_case")
+
+        self.assertEqual(string, "snake_case")
+
+    def test_convert_to_snake_case_camel_case(self):
+        string = _convert_to_snake_case("CamelCase")
+
+        self.assertEqual(string, "camel_case")
+
+    def test_convert_to_snake_case_kebab_case(self):
+        string = _convert_to_snake_case("kebab-case")
+
+        self.assertEqual(string, "kebab_case")
+
+    def test_convert_to_snake_case_spaces_numbers_and_non_alphanumeric_characters(self):
+        string = _convert_to_snake_case(" String with spaces  ")
+
+        self.assertEqual(string, "string_with_spaces")
+
+    def test_convert_to_snake_case_caps(self):
+        string = _convert_to_snake_case("stringWithCAPSAndMORE")
+
+        self.assertEqual(string, "string_with_caps_and_more")
+
+    def test_convert_to_snake_case_numbers_and_non_alphanumeric_characters(self):
+        string = _convert_to_snake_case("Num8ers & nona|phanumeric characters")
+
+        self.assertEqual(string, "num8ers_nonaphanumeric_characters")
 
 
 class ColumnReviewViewTest(TestCase):
