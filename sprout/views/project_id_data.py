654529f1aa0edaddbfa28d5bc5f4e3d8bedb81bb
Signe Kirk Brødbæk <signekb@clin.au.dk>
2024-03-18 16:32:25 +0100

    fix: remove unused import

--- sprout/views/project_id_data.py
+++ sprout/views/project_id_data.py
@@ -3,7 +3,7 @@
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import redirect, render
 
-from sprout.models import ColumnMetadata, TableMetadata
+from sprout.models import TableMetadata
 
 
 def project_id_data(
