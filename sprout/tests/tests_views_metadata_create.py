ddfbb805700604f5229a74ca8db48fdf25850eb7
Philip Hjorth Christiansen <pch@mjolner.dk>
2024-03-26 12:23:02 +0100

    test: test is adjusted to ensure the correct file and columns are uploaded

--- sprout/tests/tests_views_metadata_create.py
+++ sprout/tests/tests_views_metadata_create.py
@@ -1,8 +1,10 @@
 """Tests for the metadata create view."""
 
 import io
+from pathlib import Path
 
 from django.test import TestCase
+from django.urls import reverse
 
 from sprout.models import ColumnMetadata, FileMetadata, TableMetadata
 from sprout.tests.db_test_utils import create_table
@@ -65,21 +67,33 @@ class FileUploadTests(TestCase):
 
         self.assertContains(response, "Invalid CSV. No rows found!")
 
-    def test_resubmit_of_file_should(self):
+    def test_resubmit_of_file_should_delete_prev_file_and_columns(self):
         """Resubmitting a file table should delete the previous file and columns."""
         # Arrange
+        expected_file_content = "name,city,age\nPhil,Aarhus,36"
         table = create_table("Table Name")
         table.save()
-        file1 = self.create_file("file.csv", "name,city,age\nPhil,Aarhus,36")
-        file2 = self.create_file("file.csv", "name,city,age\nPhil,Aarhus,36")
+        file1 = self.create_file("file.csv", "first_name,year\nHans,2000")
+        file2 = self.create_file("file.csv", expected_file_content)
+        url = reverse("metadata-create", kwargs={"table_id": table.id})
 
         # Act
-        self.client.post("/metadata/create/1", {"uploaded_file": file1})
-        self.client.post("/metadata/create/1", {"uploaded_file": file2})
+        self.client.post(url, {"uploaded_file": file1})
+        self.client.post(url, {"uploaded_file": file2})
 
         # Assert
-        self.assertEqual(3, ColumnMetadata.objects.count())
-        self.assertEqual(1, FileMetadata.objects.count())
+        files = FileMetadata.objects.all()
+        columns = ColumnMetadata.objects.all()
+        self.assertEqual(1, files.count())
+        self.assertEqual(3, columns.count())
+
+        expected_columns = ["name", "city", "age"]
+        actual_columns = list(map(lambda c: c.name, columns))
+        self.assertEqual(actual_columns, expected_columns)
+
+        actual_file_content = Path(files.first().server_file_path).read_text()
+        self.assertEqual(expected_file_content, actual_file_content)
+
         # Clean up
         FileMetadata.objects.first().delete()
 
