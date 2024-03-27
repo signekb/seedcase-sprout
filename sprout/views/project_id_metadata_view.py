08e60104995aa43724b47d567e8f68d9f405dbb4
Signe Kirk Brødbæk <signekb@clin.au.dk>
2024-03-27 15:53:03 +0100

    refactor: handle redirect in template instead of view using js

--- sprout/views/project_id_metadata_view.py
+++ sprout/views/project_id_metadata_view.py
@@ -1,7 +1,7 @@
 """File with column_review view."""
 
 from django.http import HttpRequest, HttpResponse
-from django.shortcuts import redirect, render
+from django.shortcuts import render
 
 from sprout.models import TableMetadata
 
@@ -19,24 +19,11 @@ def project_id_metadata_view(request: HttpRequest) -> HttpResponse:
         to create new metadata, edit existing metadata, or upload new data.
     """
     existing_metadata = TableMetadata.objects.all()
-    selected_metadata_id = request.GET.get("selected_metadata_id")
-
-    if request.method == "POST":
-        # TODO: Add correct redirect for each button
-        if "button_create" in request.POST:
-            return redirect("/data-import")
-
-        elif "button_edit" in request.POST and selected_metadata_id is not None:
-            return redirect("/column-review/" + str(selected_metadata_id))
-
-        elif "button_upload" in request.POST and selected_metadata_id is not None:
-            return redirect("/column-review/" + str(selected_metadata_id))
 
     return render(
         request,
         "project-id-metadata-view.html",
         {
             "existing_metadata": existing_metadata,
-            "selected_metadata_id": selected_metadata_id,
         },
     )
