64415c9ee1262022d2a969a7dea765acb64197b7
Philip Hjorth Christiansen <pch@mjolner.dk>
2024-04-29 09:33:15 +0200

    feat: confirmation page

--- sprout/views/projects_id_metadata_id/step_confirmation.py
+++ sprout/views/projects_id_metadata_id/step_confirmation.py
@@ -1,9 +1,13 @@
 from django.http import HttpRequest, HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 
+from sprout.models import Tables
+
 
 def step_confirmation(
     request: HttpRequest, table_id: int
 ) -> HttpResponse | HttpResponseRedirect:
+    table = Tables.objects.prefetch_related("columns_set").get(pk=table_id)
+    context = {"table": table}
     """Renders page final page when creating metadata table."""
-    return render(request, "projects_id_metadata_id/create.html")
+    return render(request, "projects_id_metadata_id/create.html", context)
