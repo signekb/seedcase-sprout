c4d2e6d9efc629de90aae343ea1893585d57b077
martonvago <57952344+martonvago@users.noreply.github.com>
2024-08-05 16:34:32 +0200

    refactor: move some string helper functions to core (#541)

--- sprout/app/models/columns.py
+++ sprout/app/models/columns.py
@@ -1,12 +1,11 @@
 """Module defining the Columns model."""
 
-from re import sub
-
 import polars
 from django.db import models
 
 from sprout.app.models.data_types import DataTypes
 from sprout.app.models.tables import Tables
+from sprout.core.utils import convert_to_human_readable, convert_to_snake_case
 
 
 class Columns(models.Model):
@@ -35,64 +34,10 @@ class Columns(models.Model):
         return Columns(
             tables_id=table_id,
             extracted_name=series.name,
-            machine_readable_name=_convert_to_snake_case(series.name),
-            display_name=_convert_to_human_readable(series.name),
+            machine_readable_name=convert_to_snake_case(series.name),
+            display_name=convert_to_human_readable(series.name),
             description="",
             data_type=DataTypes.get_from_series(series),
             allow_missing_value=True,
             allow_duplicate_value=True,
         )
-
-
-def _convert_to_snake_case(string: str) -> str:
-    """This function takes a string and converts it to snake case.
-
-    Args:
-        string: A string to be converted to snake case
-
-    Returns:
-        A string that has been converted to snake case
-    """
-    # Remove trailing white spaces
-    altered_string = string.strip()
-
-    # Remove non-alphanumeric characters
-    altered_string = sub(r"[^a-zA-Z0-9\s_-]+", "", altered_string)
-
-    # Replace spaces and hyphens with underscores
-    altered_string = sub(r"[\s-]+", "_", altered_string)
-
-    # Convert camelCase to snake_case
-    altered_string = sub(r"([a-z0-9])([A-Z])", r"\1_\2", altered_string)
-
-    # Handle consecutive uppercase letters followed by lowercase letters (i.e., CAPS)
-    altered_string = sub(r"([A-Z])([A-Z][a-z])", r"\1_\2", altered_string)
-
-    return altered_string.lower()
-
-
-def _convert_to_human_readable(string: str) -> str:
-    """This function takes a string and converts it to human-readable title.
-
-    Examples:
-        snake_case --> Snake Case
-        PascalCase --> Pascal Case
-        CamelCase --> Camel Case
-        lower case  --> Lower Case
-        UPPER CASE --> Upper Case
-        UPPER_CASE --> Upper Case
-
-    Args:
-        string: A string to be converted
-
-    Returns:
-        A string converted to human-readable title
-    """
-    # Add space to snake_case names
-    string = sub("_", " ", string)
-
-    # Add space to CamelCase names
-    string = sub(r"([a-z])([A-Z])", r"\g<1> \g<2>", string)
-
-    # Capitalize first letter in words
-    return string.title()
--- tests/test_human_readable.py
+++ tests/test_human_readable.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 
-from sprout.app.models.columns import _convert_to_human_readable
+from sprout.core.utils import convert_to_human_readable
 
 
 class ConvertToHumanReadable(TestCase):
@@ -9,41 +9,41 @@ class ConvertToHumanReadable(TestCase):
     def test_snake_case_to_human_readable(self):
         string = "snake_case"
 
-        human_readable = _convert_to_human_readable(string)
+        human_readable = convert_to_human_readable(string)
 
         self.assertEqual("Snake Case", human_readable)
 
     def test_pascal_case_to_human_readable(self):
         string = "PascalCase"
 
-        human_readable = _convert_to_human_readable(string)
+        human_readable = convert_to_human_readable(string)
 
         self.assertEqual("Pascal Case", human_readable)
 
     def test_camel_case_to_human_readable(self):
         string = "camelCase"
 
-        human_readable = _convert_to_human_readable(string)
+        human_readable = convert_to_human_readable(string)
 
         self.assertEqual("Camel Case", human_readable)
 
     def test_lower_case_to_human_readable(self):
         string = "lower case"
 
-        human_readable = _convert_to_human_readable(string)
+        human_readable = convert_to_human_readable(string)
 
         self.assertEqual("Lower Case", human_readable)
 
     def test_upper_case_to_human_readable(self):
         string = "UPPER CASE"
 
-        human_readable = _convert_to_human_readable(string)
+        human_readable = convert_to_human_readable(string)
 
         self.assertEqual("Upper Case", human_readable)
 
     def test_upper_case_with_underscore_to_human_readable(self):
         string = "UPPER_CASE"
 
-        human_readable = _convert_to_human_readable(string)
+        human_readable = convert_to_human_readable(string)
 
         self.assertEqual("Upper Case", human_readable)
