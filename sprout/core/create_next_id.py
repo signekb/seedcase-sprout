6c4e8e1bf09ef0f0c212b346d7b67d7c49faf913
martonvago <57952344+martonvago@users.noreply.github.com>
2024-08-22 16:56:11 +0200

    feat: :sparkles: add `create_next_id()` (#585)
    ## Description
    
    This adds the `create_next_id()` function.
    
    Closes #564 
    
    ## Reviewer Focus
    
    <!-- Select quick/in-depth as necessary -->
    This PR needs a quick review.
    
    ## Checklist
    
    - [x] Added or updated tests
    - [x] Tests passed locally
    - [x] Linted and formatted code
    - [x] Build passed locally
    - [x] Updated documentation
    
    ---------
    
    Co-authored-by: Luke W. Johnston <lwjohnst86@users.noreply.github.com>

---
+++ sprout/core/create_next_id.py
@@ -0,0 +1,10 @@
+def create_next_id(ids: list[int]) -> int:
+    """Creates the next ID in a sequence, given a list of existing IDs. Starts at 1.
+
+    Args:
+        ids: A list of existing IDs.
+
+    Returns:
+        The newly generated ID.
+    """
+    return max(ids) + 1 if ids else 1
