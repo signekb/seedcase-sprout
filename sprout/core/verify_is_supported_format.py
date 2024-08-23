982c9e71c235ac1f64a70dcf808c92d66a5e5bc3
martonvago <57952344+martonvago@users.noreply.github.com>
2024-08-23 16:30:10 +0200

    feat: :sparkles: add `verify_format_supported()` (#592)
    ## Description
    
    This adds the `verify_format_supported` function.
    
    Closes #572 
    
    ## Reviewer Focus
    
    <!-- Select quick/in-depth as necessary -->
    This PR needs a quick review.
    
    ## Checklist
    
    - [x] Added or updated tests
    - [x] Tests passed locally
    - [x] Linted and formatted code
    - [x] Build passed locally
    - [x] Updated documentation

---
+++ sprout/core/verify_is_supported_format.py
@@ -0,0 +1,37 @@
+from pathlib import Path
+
+SUPPORTED_FORMATS = ["csv", "tsv", "xlsx", "xls", "json", "jsonl", "ndjson", 
+                     "geojson", "topojson", "ods", "parq", "parquet"]
+
+class UnsupportedFormatError(Exception):
+    """Raised if file format is not supported by Sprout."""
+    def __init__(self, format, *args, **kwargs):
+        """Initialises UnsupportedFormatError.
+
+        Args:
+            format: the extension of the unsupported format
+            *args: non-keyword arguments
+            **kwargs: keyword arguments
+        """
+        message = (
+            f"File format '{format}' is not supported. Sprout currently "
+            f"supports the following formats: {', '.join(SUPPORTED_FORMATS)}."
+        )        
+        super().__init__(message, *args, **kwargs)
+
+def verify_is_supported_format(path: Path) -> Path:
+    """Checks that the format of the file given by the path is supported by Sprout.
+
+    Args:
+        path: the path pointing to the file to check
+
+    Raises:
+        UnsupportedFormatError: raised if the file format is not supported
+
+    Returns:
+        the path pointing to the file, if the file format is supported
+    """
+    format = path.suffix[1:]
+    if format not in SUPPORTED_FORMATS:
+        raise UnsupportedFormatError(format)
+    return path
---
+++ tests/core/test_verify_is_supported_format.py
@@ -0,0 +1,22 @@
+from pytest import mark, raises
+
+from sprout.core.verify_is_supported_format import (
+    SUPPORTED_FORMATS,
+    UnsupportedFormatError,
+    verify_is_supported_format,
+)
+
+
+@mark.parametrize("extension", SUPPORTED_FORMATS)
+def test_accepts_supported_format(tmp_path, extension):
+    """Given a supported file format, should return the path to the file."""
+    file_path = tmp_path / f"test.{extension}"
+    assert verify_is_supported_format(file_path) == file_path
+
+
+@mark.parametrize("suffix", [".xyz", ".rtf", ".tar.gz", ".123", ".*^%#", ". ", ".", ""])
+def test_rejects_unsupported_format(tmp_path, suffix):
+    """Given an unsupported file format, should raise an UnsupportedFormatError."""
+    file_path = tmp_path / f"test{suffix}"
+    with raises(UnsupportedFormatError):
+        verify_is_supported_format(file_path)
