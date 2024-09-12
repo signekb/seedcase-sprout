79ebe4f9d5ee286add485fdfc92c9a3887921f90
Signe Kirk Brødbæk <40836345+signekb@users.noreply.github.com>
2024-09-12 16:47:39 +0200

    feat: ✨ add `get_root_envvar()` (#688)
    ## Description
    
    Closes #681 
    
    ## Reviewer Focus
    
    <!-- Select quick/in-depth as necessary -->
    This PR needs in-depth review.
    
    ## Checklist
    
    - [X] Added or updated tests
    - [X] Tests passed locally
    - [X] Linted and formatted code
    - [X] Updated documentation (=docstrings)

---
+++ tests/core/test_get_root_envvar.py
@@ -0,0 +1,18 @@
+from pathlib import Path
+
+from sprout.core.get_root_envvar import get_root_envvar
+
+
+def test_returns_root_envvar_if_set(monkeypatch):
+    """Returns SPROUT_ROOT if it is set."""
+    # Given
+    SPROUT_ROOT = "my/sprout/root"
+    monkeypatch.setenv("SPROUT_ROOT", SPROUT_ROOT)
+
+    # When, then
+    assert get_root_envvar() == Path(SPROUT_ROOT)
+
+
+def test_returns_none_if_root_envvar_is_not_set():
+    """Returns None if SPROUT_ROOT isn't set."""
+    assert get_root_envvar() is None
