6d514ff2385977afa1e4b4165e604df3623ad83a
Signe Kirk Brødbæk <40836345+signekb@users.noreply.github.com>
2024-09-13 16:30:04 +0200

    feat: ✨ add `create_root_path()` (#689)
    ## Description
    
    Closes #681 
    
    ## Reviewer Focus
    
    <!-- Select quick/in-depth as necessary -->
    This PR needs an in-depth review.
    
    ## Checklist
    
    - [X] Added or updated tests
    - [X] Tests passed locally
    - [X] Linted and formatted code
    - [X] Updated documentation (=docstrings)
    
    ---------
    
    Co-authored-by: Luke W. Johnston <lwjohnst86@users.noreply.github.com>
    Co-authored-by: martonvago <57952344+martonvago@users.noreply.github.com>

---
+++ sprout/core/create_root_path.py
@@ -0,0 +1,12 @@
+from pathlib import Path
+
+from platformdirs import user_data_path
+
+
+def create_root_path() -> Path:
+    """Creates the path to Sprout root.
+
+    Returns:
+        A Path with the Sprout root directory tied to the user.
+    """
+    return user_data_path("sprout")
---
+++ tests/core/test_create_root_path.py
@@ -0,0 +1,6 @@
+from sprout.core.create_root_path import create_root_path
+
+
+def test_returns_root_path():
+    """Returns root path."""
+    assert create_root_path().name == "sprout"
