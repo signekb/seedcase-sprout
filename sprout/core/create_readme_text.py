ca2a388c8dcc0cd0f63e29c21db05b9049602305
Kris Beicher <112945740+K-Beicher@users.noreply.github.com>
2024-10-03 14:38:28 +0200

    feat: ✨ add `create_readme_text()` (#669)
    ## Description
    
    Closes #617 
    
    ## Reviewer Focus
    
    <!-- Select quick/in-depth as necessary -->
    This PR needs an in-depth review. 
    
    I'm not sure I've picked the correct argument to pass in, or that we
    want the README file to be structured this way. Also, there is only a
    test to make sure that it works.
    
    ## Checklist
    
    - [x] Added or updated tests
    - [x] Tests passed locally
    - [x] Linted and formatted code
    - [x] Build passed locally
    - [x] Updated documentation (docstring)
    
    ---------
    
    Co-authored-by: Luke W. Johnston <lwjohnst86@users.noreply.github.com>
    Co-authored-by: martonvago <57952344+martonvago@users.noreply.github.com>

---
+++ sprout/core/create_readme_text.py
@@ -0,0 +1,22 @@
+import json
+
+
+def create_readme_text(properties: dict) -> str:
+    """Create a json object containing the readme text.
+
+    Args:
+      properties: An object containing the package and resource properties.
+
+    Returns:
+      A string for the README text
+    """
+    # TODO: Finish this once Properties have been finished.
+    # resources = # Python code to convert the resource details in the dict as
+    # a Markdown list.
+
+    readme_text = (
+        f"# {properties["name"]}: {properties["title"]}\n\n"
+        f"properties["description"]\n\n"
+        f"There are {len(properties["resources"])} resources in this package."
+    )
+    return readme_text
