7d14d5523b944496cdff33bfc159017a0ed0ddf2
martonvago <57952344+martonvago@users.noreply.github.com>
2025-01-28 13:18:57 +0000

    refactor: :recycle: use jinja template for README (#1011)
    ## Description
    
    This PR makes the README use a jinja template.
    
    I added some content, but it should obviously be refined further.
    
    With values:
    ![Screenshot from 2025-01-28
    11-27-40](https://github.com/user-attachments/assets/192f06d2-daea-4ed3-a4ac-7a8897524c35)
    
    Without values:
    ![Screenshot from 2025-01-28
    11-40-26](https://github.com/user-attachments/assets/7c7e391f-cd9a-4f41-a893-8b58f9d6e569)
    
    Closes #748 
    
    <!-- Select quick/in-depth as necessary -->
    This PR needs a medium-depth review.
    
    ## Checklist
    
    - [x] Added or updated tests
    - [x] Updated documentation
    - [x] Ran `just run-all`
    
    ---------
    
    Co-authored-by: Luke W. Johnston <lwjohnst86@users.noreply.github.com>

--- seedcase_sprout/core/create_readme_text.py
+++ seedcase_sprout/core/create_readme_text.py
@@ -1,4 +1,18 @@
-def create_readme_text(properties: dict) -> str:
+from datetime import datetime
+from importlib.resources import files
+from pathlib import Path
+
+from jinja2 import Environment, FileSystemLoader
+
+from seedcase_sprout.core.properties import (
+    LicenseProperties,
+    PackageProperties,
+)
+
+TEMPLATES_PATH: Path = files("seedcase_sprout.core").joinpath("templates")
+
+
+def create_readme_text(properties: PackageProperties) -> str:
     """Creates a string containing the README text.
 
     Args:
@@ -7,13 +21,63 @@ def create_readme_text(properties: dict) -> str:
     Returns:
       A string with the README text based on the properties.
     """
-    # TODO: Finish this once Properties have been finished.
-    # resources = # Python code to convert the resource details in the dict as
-    # a Markdown list.
+    env = Environment(loader=FileSystemLoader(TEMPLATES_PATH))
+    env.filters["join_names"] = join_names
+    env.filters["format_date"] = format_date
+    env.filters["inline_code"] = inline_code
+    env.filters["format_link"] = format_link
+    template = env.get_template("README.jinja2")
+    return template.render(properties=properties)
 
-    readme_text = (
-        f"# {properties['name']}: {properties['title']}\n\n"
-        f"{properties['description']}\n\n"
-        f"There are {len(properties['resources'])} resources in this package."
+
+def join_names(licenses: list[LicenseProperties] | None) -> str:
+    """Joins license names into a comma-separated list.
+
+    Args:
+        licenses: The licenses.
+
+    Returns:
+        A comma-separated list of names.
+    """
+    return ", ".join(license.name for license in licenses) if licenses else "N/A"
+
+
+def format_date(created: str | None) -> str:
+    """Transforms ISO date stamp to human-readable format.
+
+    Args:
+        created: The ISO date stamp.
+
+    Returns:
+        The date in a human-readable format.
+    """
+    return (
+        datetime.fromisoformat(created).strftime("%d %B %Y, %H:%M")
+        if created
+        else "N/A"
     )
-    return readme_text
+
+
+def inline_code(value: str | None) -> str:
+    """Adds inline code formatting to the input.
+
+    Args:
+        value: The value to format as inline code.
+
+    Returns:
+        The value formatted as inline code.
+    """
+    return f"`{value}`" if value else "N/A"
+
+
+def format_link(url: str | None, title: str = "See more") -> str:
+    """Formats a URL as a markdown link.
+
+    Args:
+        url: The URL to format.
+        title: The title to show for the link. Defaults to "See more".
+
+    Returns:
+        A markdown link using the URL and the title.
+    """
+    return f"[{title}]({url})" if url else "N/A"
