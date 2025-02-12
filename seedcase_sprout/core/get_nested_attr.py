58003ed3603520073975c542f62abf1aedad8135
martonvago <57952344+martonvago@users.noreply.github.com>
2025-02-12 12:38:38 +0000

    feat: :sparkles: add `get_nested_attr()` (#1039)
    ## Description
    
    This PR adds a utility function for making it easier to access nested
    properties on objects.
    Our Properties class structure has lots of nesting and all attributes
    default to None, so when I want to access a nested attribute (e.g.
    `resource_properties.dialect.header`) I always have to make sure it
    doesn't throw an error.
    
    With this function, we can instead say
    `get_nested_attr(resource_properties, "dialect.header", default=True)`,
    optionally providing a default value. Similar to the built-in `getattr`.
    
    <!-- Select quick/in-depth as necessary -->
    This PR needs a quick review.
    
    ## Checklist
    
    - [x] Added or updated tests
    - [x] Updated documentation
    - [x] Ran `just run-all`

---
+++ seedcase_sprout/core/get_nested_attr.py
@@ -0,0 +1,51 @@
+from typing import Any, TypeVar
+
+T = TypeVar("T")
+
+
+def get_nested_attr(
+    base_object: Any, attributes: str, default: T | None = None
+) -> T | None:
+    """Returns the attribute specified by `attributes`.
+
+    Tries to resolve the chain of attributes against `base_object`. Returns None
+    or the specified default value if the attribute chain cannot be resolved.
+
+    Args:
+        base_object: The object to start resolving the attributes from.
+        attributes: The chain of attributes as a dot-separated string.
+        default: The default value to return if the attributes cannot be resolved.
+            Defaults to None.
+
+    Returns:
+        The value at the end of the attribute chain.
+
+    Raises:
+        ValueError: If the attribute chain contains an element that is not a valid
+            identifier.
+
+    Examples:
+        ```{python}
+        class Inner:
+            pass
+        class Middle:
+            inner: Inner = Inner()
+        class Outer:
+            middle: Middle = Middle()
+
+        get_nested_attr(Outer(), "middle.inner")
+        ```
+    """
+    attributes = attributes.split(".")
+    if any(not attribute.isidentifier() for attribute in attributes):
+        raise ValueError(
+            "`attributes` should contain valid identifiers separated by `.`."
+        )
+
+    try:
+        for attribute in attributes:
+            base_object = getattr(base_object, attribute)
+    except AttributeError:
+        return default
+
+    return default if base_object is None else base_object
