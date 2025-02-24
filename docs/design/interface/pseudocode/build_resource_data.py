394d22abaeaab1f166acb98647f1e48eec2cfc1f
Luke W. Johnston <lwjohnst86@users.noreply.github.com>
2025-02-24 09:42:36 +0100

    docs: :building_construction: revise to `build_resource_data()` to output a DataFrame (#1074)
    ## Description
    
    After seeing @K-Beicher working with the data and making the guide, it
    felt off to immediately save to Parquet. Given this is `core` and
    developer-focused/highly flexible, I can imagine situations where there
    may be some post-processing before generating to the Parquet file. So
    I've modified this function to output to a DataFrame rather than save
    directly to Parquet.
    
    <!-- Select quick/in-depth as necessary -->
    This PR needs an in-depth review.
    
    ---------
    
    Co-authored-by: Signe Kirk Brødbæk <40836345+signekb@users.noreply.github.com>

---
+++ docs/design/interface/pseudocode/build_resource_data.py
@@ -0,0 +1,69 @@
+# ruff: noqa
+def build_resource_data(
+    raw_files_path: list[Path], resource_properties: ResourceProperties
+) -> DataFrame:
+    """Merge all raw resource file(s) into a single (Polars) DataFrame.
+
+    This function takes the file(s) given by `raw_files_path`, merges them
+    together, does some checks and minor processing before outputting them as a
+    single (Polars) DataFrame. The `resource_properties` object is used to check the data
+    and ensure it is correct. This function doesn't write to Parquet, so that you can apply any additional
+    processing to the data before saving it as a Parquet file.  While Sprout
+    generally assumes that the files stored in the `resources/<id>/raw/` folder
+    are already correctly structured and tidy, this function still runs checks to ensure
+    the data are correct by comparing to the properties. All data in the
+    `resources/<id>/raw/` folder will be merged into one single DataFrame object
+    for either continued processing or to write to the Parquet file.
+
+    If there are any duplicate observational units in the data, only the most recent
+    observational unit will be kept. This way, if there are any errors or mistakes
+    in older raw files that have been corrected in later files, the mistake can still
+    be kept, but won't impact the data that will actually be used.
+
+    Examples:
+
+        ``` python
+        import seedcase_sprout.core as sp
+
+        sp.build_resource_data(
+            raw_files_path=sp.path_resources_raw_files(1),
+            resource_properties=sp.example_resource_properties,
+        )
+        ```
+
+    Args:
+        raw_files_path: A list of paths for all the raw files, mostly commonly
+            stored in the `.csv.gz` format. Use `path_resource_raw_files()` to help
+            provide the correct paths to the raw files.
+        resource_properties: The `ResourceProperties` object that contains the properties
+            of the resource you want to check the data against.
+
+    Returns:
+        Outputs a single DataFrame object of all the merged raw files.
+    """
+    # Not sure if this is the correct way to verify multiple files.
+    [check_is_file(path) for path in raw_files_path]
+    check_resource_properties(resource_properties)
+
+    data = read_raw_files(raw_files_path)
+    data = drop_duplicate_obs_units(data)
+
+    # This function could be several functions or the one full function.
+    check_data(data, resource_properties)
+
+    return data
+
+
+def read_raw_files(paths: list[Path]) -> DataFrame:
+    # Can read gzip files.
+    data_list = [polars.read_csv(path) for path in paths]
+    # Merge them all together.
+    data = polars.concat(data_list)
+    return data
+
+
+def drop_duplicate_obs_units(data: DataFrame) -> DataFrame:
+    # Drop duplicates based on the observation unit, keeping only the most
+    # recent one. This allows older raw files to contain potentially wrong
+    # data that was corrected in the most recent file.
+    return data.drop_duplicates()
