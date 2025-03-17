bfccdecf79408965bc10ac90cdfaecf2bd5e5283
martonvago <57952344+martonvago@users.noreply.github.com>
2025-03-17 14:38:08 +0000

    feat: :sparkles: add `set_missing_values_to_null()` (#1131)
    ## Description
    
    This PR adds a function for setting missing values (as set by the user
    in the resource properties) to null. Relevant Frictionless docs
    [here](https://datapackage.org/standard/table-schema/#missingValues) and
    [here](https://datapackage.org/standard/table-schema/#field-missingValues).
    I expect this to be used used when checking data and also when
    converting to Parquet possibly.
    
    <!-- Select quick/in-depth as necessary -->
    This PR needs an in-depth review.
    
    ## Checklist
    
    - [x] Added or updated tests
    - [x] Updated documentation
    - [x] Ran `just run-all`
    
    ---------
    
    Co-authored-by: Luke W. Johnston <lwjohnst86@users.noreply.github.com>

---
+++ src/seedcase_sprout/core/set_missing_values_to_null.py
@@ -0,0 +1,42 @@
+import polars as pl
+
+from seedcase_sprout.core.get_nested_attr import get_nested_attr
+from seedcase_sprout.core.properties import (
+    FieldProperties,
+    ResourceProperties,
+)
+
+
+def set_missing_values_to_null(
+    data: pl.DataFrame, resource_properties: ResourceProperties
+):
+    """Sets missing values to null.
+
+    Uses the resource properties to locate missing values in the data frame and sets
+    them to null.
+
+    Args:
+        data: The data frame to process.
+        resource_properties: The resource properties describing the data.
+
+    Returns:
+        The updated data frame with missing values set to null.
+    """
+    fields: list[FieldProperties] = get_nested_attr(
+        resource_properties, "schema.fields", default=[]
+    )
+
+    schema_missing_values = get_nested_attr(
+        resource_properties, "schema.missing_values", default=[""]
+    )
+    return data.with_columns(
+        pl.col(field.name).replace(
+            old=schema_missing_values
+            if field.missing_values is None
+            # As per Frictionless standard, field-level missing values overrides
+            # the schema-level missing values.
+            else field.missing_values,
+            new=None,
+        )
+        for field in fields
+    )
