8ad129ce7a8ba4deb4b48a7a37fd25d77dac51f8
Luke W. Johnston <lwjohnst86@users.noreply.github.com>
2025-03-19 09:37:12 +0100

    docs: :memo: revise from `raw` to `batch` in design docs (#1142)
    ## Description
    
    As per our discussions, I've renamed all raw to batch in design docs and
    pseudocode.
    
    Closes #1128
    
    <!-- Select quick/in-depth as necessary -->
    This PR needs an in-depth review.

--- docs/design/interface/pseudocode/build_resource_data.py
+++ docs/design/interface/pseudocode/build_resource_data.py
@@ -1,23 +1,24 @@
 # ruff: noqa
 def build_resource_data(
-    raw_files_path: list[Path], resource_properties: ResourceProperties
+    batch_files_path: list[Path], resource_properties: ResourceProperties
 ) -> DataFrame:
-    """Merge all raw resource file(s) into a single (Polars) DataFrame.
+    """Merge all batch resource file(s) into a single (Polars) DataFrame.
 
-    This function takes the file(s) given by `raw_files_path`, merges them
+    This function takes the file(s) given by `batch_files_path`, merges them
     together, does some checks and minor processing before outputting them as a
-    single (Polars) DataFrame. The `resource_properties` object is used to check the data
-    and ensure it is correct. This function doesn't write to Parquet, so that you can apply any additional
-    processing to the data before saving it as a Parquet file.  While Sprout
-    generally assumes that the files stored in the `resources/<id>/raw/` folder
-    are already correctly structured and tidy, this function still runs checks to ensure
-    the data are correct by comparing to the properties. All data in the
-    `resources/<id>/raw/` folder will be merged into one single DataFrame object
-    for either continued processing or to write to the Parquet file.
+    single (Polars) DataFrame. The `resource_properties` object is used to check
+    the data and ensure it is correct. This function doesn't write to Parquet,
+    so that you can apply any additional processing to the data before saving it
+    as a Parquet file.  While Sprout generally assumes that the files stored in
+    the `resources/<id>/batch/` folder are already correctly structured and
+    tidy, this function still runs checks to ensure the data are correct by
+    comparing to the properties. All data in the `resources/<id>/batch/` folder
+    will be merged into one single DataFrame object for either continued
+    processing or to write to the Parquet file.
 
     If there are any duplicate observational units in the data, only the most recent
     observational unit will be kept. This way, if there are any errors or mistakes
-    in older raw files that have been corrected in later files, the mistake can still
+    in older batch files that have been corrected in later files, the mistake can still
     be kept, but won't impact the data that will actually be used.
 
     Examples:
@@ -26,26 +27,26 @@ def build_resource_data(
         import seedcase_sprout.core as sp
 
         sp.build_resource_data(
-            raw_files_path=sp.path_resources_raw_files(1),
+            batch_files_path=sp.path_resources_batch_files(1),
             resource_properties=sp.example_resource_properties,
         )
         ```
 
     Args:
-        raw_files_path: A list of paths for all the raw files, mostly commonly
-            stored in the `.csv.gz` format. Use `path_resource_raw_files()` to help
-            provide the correct paths to the raw files.
+        batch_files_path: A list of paths for all the batch files, mostly commonly
+            stored in the `.csv.gz` format. Use `path_resource_batch_files()` to help
+            provide the correct paths to the batch files.
         resource_properties: The `ResourceProperties` object that contains the properties
             of the resource you want to check the data against.
 
     Returns:
-        Outputs a single DataFrame object of all the merged raw files.
+        Outputs a single DataFrame object of all the merged batch files.
     """
     # Not sure if this is the correct way to verify multiple files.
-    [check_is_file(path) for path in raw_files_path]
+    [check_is_file(path) for path in batch_files_path]
     check_resource_properties(resource_properties)
 
-    data = read_raw_files(raw_files_path)
+    data = read_batch_files(batch_files_path)
     data = drop_duplicate_obs_units(data)
 
     # This function could be several functions or the one full function.
@@ -54,7 +55,7 @@ def build_resource_data(
     return data
 
 
-def read_raw_files(paths: list[Path]) -> DataFrame:
+def read_batch_files(paths: list[Path]) -> DataFrame:
     # Can read gzip files.
     data_list = [polars.read_csv(path) for path in paths]
     # Merge them all together.
@@ -64,6 +65,6 @@ def read_raw_files(paths: list[Path]) -> DataFrame:
 
 def drop_duplicate_obs_units(data: DataFrame) -> DataFrame:
     # Drop duplicates based on the observation unit, keeping only the most
-    # recent one. This allows older raw files to contain potentially wrong
+    # recent one. This allows older batch files to contain potentially wrong
     # data that was corrected in the most recent file.
     return data.drop_duplicates()
--- docs/design/interface/pseudocode/write_resource_data_to_raw.py
+++ docs/design/interface/pseudocode/write_resource_data_to_raw.py
@@ -1,13 +1,13 @@
 # ruff: noqa
-def write_resource_data_to_raw(data_path, resource_properties) -> Path:
-    """Writes the raw data file into the resource's raw data folder.
+def write_resource_batch_data(data_path, resource_properties) -> Path:
+    """Writes the original data file into the resource's batch data folder.
 
     Copy the file from `data_path` over into the resource location given by
     `path`. This will compress the file and use a timestamped, unique file
     name to store it as a backup. See the
     [design](https://sprout.seedcase-project.org/docs/design/) docs for an
     explanation of this file. Data is always checked against the properties
-    before saving into the raw folder. Copies and compresses the file, and
+    before saving into the batch folder. Copies and compresses the file, and
     outputs the path object of the created file.
 
         Args:
@@ -18,7 +18,7 @@ def write_resource_data_to_raw(data_path, resource_properties) -> Path:
                 and `get_properties()` to get the correct resource properties.
 
         Returns:
-            The path to the written compressed raw resource file.
+            The path to the written Parquet resource file.
 
         Examples:
             ```{python}
@@ -27,91 +27,32 @@ def write_resource_data_to_raw(data_path, resource_properties) -> Path:
     """
     check_is_file(data_path)
     check_is_supported_format(data_path)
-    check_data_basics(data_path, resource_properties)
-    check_data_constraints(data_path, resource_properties)
+    data = read_data(data_path)
+    check_data(data, resource_properties)
     # Since `path` is to the `data.parquet` file.
-    raw_dir = Path(resource_properties.path.parent / "raw")
+    batch_dir = Path(resource_properties.path.parent / "batch")
 
-    raw_resource_path = Path(raw_dir / create_raw_file_name(data_path))
-    return write_compressed_file(data_path, raw_resource_path)
+    batch_resource_path = Path(batch_dir / create_batch_file_name(data_path))
+    return write_parquet(data_path, batch_resource_path)
 
 
-def create_raw_file_name(data_path: Path) -> str:
-    """Creates a timestamped, unique file name for the raw data file.
+def create_batch_file_name(data_path: Path) -> str:
+    """Creates a timestamped, unique file name for the batch data file.
 
-    This function creates a timestamped, unique file name for the raw data file
-    that is being copied into the resource's raw data folder. This file name
-    is used to store the raw data file as a backup. The file name will be
+    This function creates a timestamped, unique file name for the batch data file
+    that is being copied into the resource's batch data folder. This file name
+    is used to store the data file as a backup. The file name will be
     in the format `{timestamp}-{uuid}.{extension}.gz`, where `timestamp` is the
     current time following ISO8601 format, and `uuid` is a universally unique ID.
 
         Args:
-            data_path: The path to the raw data file.
+            data_path: The path to the batch data file.
 
         Returns:
-            A path to a (potentially) new raw resource file.
+            A path to a (potentially) new batch resource file.
     """
     # Untested code.
     timestamp = datetime.datetime.now().isoformat()
     uuid = str(uuid.uuid4())
     extension = data_path.suffix
     return f"{timestamp}-{uuid}.{extension}.gz"
-
-
-def write_compressed_file(data_path: Path, path: Path) -> Path:
-    """Compress and write the raw data file into the resource's raw data folder.
-
-    This function copies the raw data file from `data_path` to the resource's
-    raw data folder, compressing it in the process. The compressed file is
-    stored in the raw data folder based on the path provided.
-
-        Args:
-            data_path: The path to the raw data file.
-            path: The path to the raw resource file in the
-                resource's raw data folder.
-
-        Returns:
-            The path to the written compressed raw resource file.
-    """
-    return path
-
-
-def check_data_basics(data_path: Path, resource_properties: ResourceProperties) -> Path:
-    """Basic checks on the data against the resource properties.
-
-    Checks the data for basic things, including against the specific resource's
-    properties for things like:
-
-    - Can it be at a minimal read without problems or warnings?
-    - Do the columns in the data file match those in the properties?
-    - Do the data types in the data file match those in the properties?
-
-        Args:
-            data_path: The path to the raw data file.
-            resource_properties: The properties object for the specific resource.
-
-        Returns:
-            An error if the requirements are not met.
-    """
-    return None
-
-
-def check_data_constraints(
-    data_path: Path, resource_properties: ResourceProperties
-) -> Path:
-    """Check the data against the resource's constraints properties.
-
-    The resource properties sometimes contains a `constraints` property
-    for each column. This checks whether the data matches the constraints
-    given in the properties.
-
-        Args:
-            data_path: The path to the raw data file.
-            resource_properties: The properties object for the specific resource.
-
-        Returns:
-            An error if the constraints are not met, otherwise `None`.
-    """
-    # Check the data against the resource constraints properties.
-
-    return None
