43f1c204252dcd07bead3d11b69ad974d61450c9
Luke W. Johnston <lwjohnst86@users.noreply.github.com>
2025-03-20 11:52:50 +0100

    chore: :fire: these are not used, so deleting (#1158)
    ## Description
    
    These are unused functions, so deleting them.
    
    Closes #811 
    
    <!-- Select quick/in-depth as necessary -->
    This PR needs a quick review.
    
    ## Checklist
    
    - [x] Ran `just run-all`

--- tests/core/test_create_readme_path.py
+++
@@ -1,6 +0,0 @@
-from seedcase_sprout.core.create_readme_path import create_readme_path
-
-
-def test_returns_path_to_readme(tmp_path):
-    """Given a folder, should return the correct path to the README."""
-    assert create_readme_path(tmp_path) == tmp_path / "README.md"
