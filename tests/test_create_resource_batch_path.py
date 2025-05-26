b20c9890516b34cfefe3871fffbf39e2eb1a525d
Luke W. Johnston <lwjohnst86@users.noreply.github.com>
2025-05-26 11:50:12 +0200

    chore: :fire: remove leftover code (#1341)

--- tests/test_create_resource_batch_path.py
+++
@@ -1,6 +0,0 @@
-from seedcase_sprout.create_resource_batch_path import create_resource_batch_path
-
-
-def test_outputs_correct_batch_directory_path(tmp_path):
-    """The batch directory path is output correctly."""
-    assert create_resource_batch_path(tmp_path) == tmp_path / "batch"
